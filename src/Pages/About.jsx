594981befe42e5db780da337499cb319d72e9b92
mohammed sameer ahmed <sameerahmed976@gmail.com>
2022-09-26 12:57:56 +0530

    added about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="about">
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+        corporis minus necessitatibus voluptatum velit vel at ipsam veniam in
+        dolorum! Nam praesentium eos unde illo deleniti tempore, cupiditate ab
+        cumqus dolorum id consequuntur odio repudiandae aliquid architecto
+        veritatis tempora quis quo non ea sit ip delectus doloremque libero
+        minima facilis. Excepturi illum repudiandae consectetur? Voluptatum
+        consectetur ab in quidem laboriosam repudiandae sit neque!
+      </p>
+    </section>
+  );
 };
 
 export default About;
